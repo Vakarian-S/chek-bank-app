cb7bdff5b246a80d590876f066e14269e2ae8375
Sebastian <sebastian.gvg94@gmail.com>
2022-06-06 21:58:18 -0400

    Feat(Deposit): Implement deposit view

--- src/app/components/deposit-new/deposit-new.module.ts
+++ src/app/components/deposit-new/deposit-new.module.ts
@@ -4,6 +4,12 @@ import { CommonModule } from '@angular/common';
 import { DepositNewRoutingModule } from './deposit-new-routing.module';
 import { DepositNewComponent } from './pages/deposit-new.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -13,7 +19,13 @@ import { MatCardModule } from '@angular/material/card';
   imports: [
     CommonModule,
     DepositNewRoutingModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatSnackBarModule,
+    ReactiveFormsModule,
+    MatButtonModule
   ]
 })
 export class DepositNewModule { }
